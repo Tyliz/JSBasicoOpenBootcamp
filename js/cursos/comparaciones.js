46b769dbea70db63ed9b9aff907bd423547f3fea
Tyliz <caosavenger@gmail.com>
2023-02-11 16:59:36 -0400

    Clase 3

---
+++ js/cursos/comparaciones.js
@@ -0,0 +1,4 @@
+console.log('5' == 5); // resultado: True => igualdad de valor
+console.log('5' === 5); // resultado: False => igualdad estricta incluye el tipo
+console.log('5' != 5); // resultado: False => desigualdad de valor
+console.log('5' !== 5); // resultado: True => desigualdad estricta incluye el tipo
