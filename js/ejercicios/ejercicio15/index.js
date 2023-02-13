bb36064a23ad1d691bb50858fb62b558252daa59
Tyliz <caosavenger@gmail.com>
2023-02-13 02:16:09 -0400

    Ejercicio 15: Depuración

---
+++ js/ejercicios/ejercicio15/index.js
@@ -0,0 +1,25 @@
+// // Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+
+// // Ejecuta la depuración de VSCode para visualizar la ejecución de la función
+
+function Fibonacci(num) {
+	let lstSecuencia = [1, 1];
+	if (num < 1)
+		return [];
+	if (num == 1)
+		return [1];
+
+	for (let i = 2; i < num; i++) {
+		lstSecuencia.push(lstSecuencia[i - 1] + lstSecuencia[i - 2]);
+	}
+	return lstSecuencia;
+}
+function Fibonacci2(num) {
+	let lstSecuencia = [1, 1];
+	for (let i = 2; i < num; i++) {
+		lstSecuencia.push(lstSecuencia[i - 1] + lstSecuencia[i - 2]);
+	}
+	return ((num < 1) && (lstSecuencia = [])) || ((num == 1) && (lstSecuencia = [1])) || lstSecuencia;
+}
+
+console.log(Fibonacci2(6));
